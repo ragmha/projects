57372609227e2f4c15e4cb6e527a6b26b6f109f6
ragmha <raghibmidhat@gmail.com>
2021-10-16 21:23:41 +0300

    add: redux-starter

---
+++ packages/redux-starter/src/components/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1> 🦄 </h1>
+          <h1>redux-starter</h1>
+        </div>
+        <p>
+          Edit <strong> src/components/App.js </strong>
+          to get started...🚀
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
