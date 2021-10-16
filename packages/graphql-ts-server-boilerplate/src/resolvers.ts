deda8417e3a1eadb97edfcf71acb95d6baecd015
ragmha <raghibmidhat@gmail.com>
2021-10-16 21:49:49 +0300

    add: gql-ts-server-boilerplate

---
+++ packages/graphql-ts-server-boilerplate/src/resolvers.ts
@@ -0,0 +1,23 @@
+import * as bcrypt from 'bcryptjs';
+
+import { User } from './entity/User';
+import { ResolverMap } from './types/graphql-utils';
+import { GQL } from './types/schema.d';
+
+export const resolvers: ResolverMap = {
+  Query: {
+    hello: (_, { name }: GQL.IHelloOnQueryArguments) =>
+      `Bye ${name || 'World'}`,
+  },
+  Mutation: {
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({ email, password: hashedPassword });
+      await user.save();
+      return true;
+    },
+  },
+};
